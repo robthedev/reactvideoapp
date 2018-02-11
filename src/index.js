81fc892a81983a1ae3d1005d779403b09fce7c0c
robthewebdev <robthewebdev@gmail.com>
2018-02-10 22:57:30 -0600

    added new files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
 //create component, should make some html
 
+const YOUTUBE_API_KEY = 'AIzaSyDhuROJa0X-ayqVc-ndYWQCb1CWR9C_dIY';
+
 const App = () => {
-    return <div>TExt</div>;
-}
+    return (
+        <div>
+          <SearchBar />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
